b0e5618010ac80ba964613998aab7916e032d1b8
GOKUL_REDDY <62810166+gokulsankar706@users.noreply.github.com>
2022-09-19 12:47:30 +0530

    Update Staticfactory.java

--- Staticfactory.java
+++ Staticfactory.java
@@ -6,17 +6,34 @@ public class Staticfactory {
 	private int age;
 	private float height;
 	
-	private Staticfactory() {
-		
-	}
+	private Staticfactory() {}
+	
 	private Staticfactory(String name, int age, float height) {
 		this.name = name;
 		this.age = age;
 		this.height = height;
 	}
+	private Staticfactory(String name) {
+		this.name = name;
+	}
+	private Staticfactory(int age) {
+		this.age = age;
+	}
+	private Staticfactory(float height) {
+		this.height = height;
+	}
 	
 	public static Staticfactory setValues(String name, int age, float height) {
 		return new Staticfactory(name,age,height);
+	}
+	public static Staticfactory setName(String name) {
+		return new Staticfactory(name);
+	}
+	public static Staticfactory setAge(int age) {
+		return new Staticfactory(age);
+	}
+	public static Staticfactory setAge( float height) {
+		return new Staticfactory(height);
 	}
 
 	@Override
